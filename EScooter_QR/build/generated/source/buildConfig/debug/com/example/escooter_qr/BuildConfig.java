5ff7ac830a6e52cb85beadc171ca099dc7c5b8b2
BandarSH <85843991+BandarSH@users.noreply.github.com>
2022-12-13 06:44:49 +0300

    Add files via upload

---
+++ EScooter_QR/build/generated/source/buildConfig/debug/com/example/escooter_qr/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.escooter_qr;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.escooter_qr";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
